85f6892a26154b4a5e41af85efc5463a8f206986
VezRoland <vezrolaa@gmail.com>
2024-03-13 08:36:08 +0100

    compare függvény

--- utils.js
+++ utils.js
@@ -1,3 +1,9 @@
-function genarateRandomNr(minNr,maxNr){
+export function genarateRandomNr(minNr,maxNr){
     return Math.floor(Math.random() * (maxNr - minNr+1) ) + minNr;
+}
+
+export function compare(guessedNr, randomNr) {
+	if (guessedNr === randomNr) return 'Helyes megfejtés.'
+	if (guessedNr < randomNr) return 'A te számod kisebb.'
+	if (guessedNr > randomNr) return 'A te számod nagyobb.'
 }
